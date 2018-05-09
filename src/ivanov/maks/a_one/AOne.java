f0eb939a11f1d777e2a332a4333353a8f4ed717f
Max1vanov <102-59@mail.ru>
2018-05-09 16:15:01 +0500

    #1 Exercise 1

---
+++ src/ivanov/maks/a_one/AOne.java
@@ -0,0 +1,8 @@
+package ivanov.maks.a_one;
+
+public class AOne {
+    public static void main(String[] args) {
+        NameMessage name = new NameMessage();
+        name.Message();
+    }
+}
---
+++ src/ivanov/maks/a_one/NameMessage.java
@@ -0,0 +1,14 @@
+package ivanov.maks.a_one;
+
+import java.util.Scanner;
+
+class NameMessage {
+
+     void Message() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+
+        System.out.println("Your name is " + scanner.next());
+        scanner.close();
+    }
+}
