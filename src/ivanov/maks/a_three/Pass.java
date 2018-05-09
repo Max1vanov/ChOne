b87dbbf14eff6acc959d0294079baf102482baa8
Max1vanov <102-59@mail.ru>
2018-05-09 16:57:55 +0500

    #3 Exercise 4

---
+++ src/ivanov/maks/a_three/Pass.java
@@ -0,0 +1,28 @@
+package ivanov.maks.a_three;
+
+import java.util.Scanner;
+
+class Pass {
+    private String pass;
+
+    {
+        pass = "12345";
+    }
+
+    private String yourPass;
+    private Scanner scanner = new Scanner(System.in);
+
+    void enterYourPass() {
+        System.out.print("Enter your password: ");
+        yourPass = scanner.next();
+        scanner.close();
+    }
+
+    void passEqual() {
+        if (pass.equals(yourPass)) {
+            System.out.println("Hello!");
+        } else {
+            System.err.println("Wrong password!");
+        }
+    }
+}
