bd4127fa0c0fa769bed66cb0881e65e0248344f5
Max1vanov <102-59@mail.ru>
2018-05-09 16:32:15 +0500

    #2 Exercise 2

---
+++ src/ivanov/maks/a_two/ATwo.java
@@ -0,0 +1,10 @@
+package ivanov.maks.a_two;
+
+public class ATwo {
+    public static void main(String[] args) {
+        ComandLine comandLine = new ComandLine();
+        comandLine.showLine(args);
+        comandLine.line(args);
+        comandLine.showLine(args);
+    }
+}
